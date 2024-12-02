91eaf0df80ed3469707da25b1f10e336f9de9ab2
Abdullah <abdullahlaptop6445@gmail.com>
2024-12-02 13:34:37 +0500

    firebase integration done and crashlytics completed

---
+++ lib/main_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: TextButton(
+          onPressed: () => throw Exception(),
+          child: const Text("Throw Test Exception"),
+        ),
+      ),
+    );
+  }
+}
